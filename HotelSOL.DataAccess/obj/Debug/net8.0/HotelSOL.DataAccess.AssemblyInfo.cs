d9dea41c81bbe9307957bc329a46376d1155d02a
Marc <163986617+mcampuzanog@users.noreply.github.com>
2025-05-25 12:53:07 +0200

    menu

--- HotelSOL.DataAccess/obj/Debug/net8.0/HotelSOL.DataAccess.AssemblyInfo.cs
+++ HotelSOL.DataAccess/obj/Debug/net8.0/HotelSOL.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelSOL.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8afacc422ec07460994904952714fdad0e7f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404619da4c898322a07bd94257e61cf553a37e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelSOL.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelSOL.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
